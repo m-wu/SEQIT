56b1bd7af71a74396bc6cd8983a882e7f9377513
Mike Wu <m@mikewu.net>
2014-11-13 11:50:50 -0800

    Add support for show/hide individual trials
    - populate the trial view from the input JSON file
    - modified the format of the JSON file to allow multiple trials and
    include trial information

--- js/eyegazevis.js
+++ js/eyegazevis.js
@@ -1,33 +1,61 @@
+var datasets = new Array();
+
+var mainviewsvg;
+
 var canvas_width = 900;
 var canvas_height = 719;
 
-var dataset;
 
 var mainview_width = 900;
 var mainview_height = 719;
 var mainview_bg_opacity = 0.5;
+
+var zoom_ratio = 0.7035; // image coordinate / fixation coordinate
 
 $(window).load(function() {
   main();
 });
 
 function main(){
-  d3.json("trial1.json", function(error, json){
-    dataset = json;
+  d3.json("datasets.json", function(error, json){
+    for (var i in json){
+      var id = json[i].user.concat(json[i].task);
+      datasets[id] = json[i].fixpoints;
+      addButtonToTrialView(id);
+    }
     drawMainView();
+  })
+}
+
+function addButtonToTrialView(id){  
+  d3.select("#user-group")
+    .append("button")
+    .attr("type", "button")
+    .attr("id", id)
+    .attr("class", "btn btn-primary")
+    .attr("autocomplete","off")
+    .text(id);
+
+  $('#'.concat(id)).on('click', function () {
+    if (!$(this).hasClass('active')){
+      drawFixationPoints(mainviewsvg, id);
+      // drawScanPath(mainviewsvg, datasets["user1task15"]);
+    } else {
+      removeFixationPoints(mainviewsvg, "#".concat(id));
+    }
   })
 }
 
 function drawMainView(){
   // Create SVG element
-  var svg = d3.select("#mainview")
+  mainviewsvg = d3.select("#mainview")
               .append("svg")
               .attr("width", canvas_width)
               .attr("height", canvas_height);
 
   // Add background image to the main view
   // Define the image as a pattern
-  svg.append("defs")
+  mainviewsvg.append("defs")
     .append("pattern")
       .attr("id", "mainviewbg")
       .attr('width', mainview_width)
@@ -39,37 +67,43 @@ function drawMainView(){
         .attr('height', mainview_height);
 
   // Fill a rectangle with the pattern
-  svg.append("rect")
+  mainviewsvg.append("rect")
     .attr("fill", "url(#mainviewbg)")
     .attr('width', mainview_width)
     .attr('height', mainview_height)
     .attr('fill-opacity', mainview_bg_opacity);
+}
 
+function drawFixationPoints(svg, dataset_id){
   // Draw fixation points
-  svg.selectAll("circle")
-    .data(dataset)
+  svg.append("g")
+      .attr("id", dataset_id)
+    .selectAll("circle")
+    .data(datasets[dataset_id])
     .enter()
     .append("circle")
       .attr("cx", function(d) {
-        return 0.7035*d.x;
+        return zoom_ratio*d.x;
       })
       .attr("cy", function(d) {
-        return 0.7035*d.y;
+        return zoom_ratio*d.y;
       })
       .attr("r", 5);
+}
 
+function removeFixationPoints(svg, group_id){
+  svg.select(group_id).remove();
+}
 
-  // Draw scan path
+function drawScanPath(svg, dataset){
   var scanpathFunction = d3.svg.line()
-                           .x(function(d) { return 0.7035*d.x; })
-                           .y(function(d) { return 0.7035*d.y; })
-                           .interpolate("linear");
+                         .x(function(d) { return zoom_ratio*d.x; })
+                         .y(function(d) { return zoom_ratio*d.y; })
+                         .interpolate("linear");
 
   svg.append("path")
      .attr("d", scanpathFunction(dataset))
      .attr("stroke", "blue")
      .attr("stroke-width", 2)
      .attr("fill", "none");
-
 }
-
